0c3d158b0bc66c09662e9b2c7352e64d25c9c0ab
mwaughhpz640 <waugh.e.michael@gmail.com>
2019-08-14 08:34:45 -0400

    updates

--- prerevision_code/pref_shock/calibrate_fmin.m
+++ prerevision_code/pref_shock/calibrate_fmin.m
@@ -16,9 +16,11 @@ fval = zeros(niters,1);
 for zzz = 1:niters
 rng(08182016 + zzz)
 
-[new_val(zzz,:),fval(zzz)]= fminsearch(@(xxx)calibrate_model(exp(xxx),1),log(guess) + .1*randn(1,length(guess)),options);
+[new_val(zzz,:),fval(zzz)]= fminsearch(@(xxx)calibrate_model(exp(xxx),1),log(guess) + .01*randn(1,length(guess)),options);
 
 end
 
 disp(new_val)
 %compute_outcomes_prefshock(exp(new_val),1);
+
+save calibration_rand new_val fval
