907dd43e9bd30308263b69b38c268a7cf92bdbd7
mikew <waugh.e.michael@gmail.com>
2021-04-14 16:44:18 -0400

    removed test files

--- revision2_code/calibration/preamble.m
+++ revision2_code/calibration/preamble.m
@@ -45,7 +45,7 @@ if isempty(specs)
     %specs.asset_space = linspace(0,2,100); % this is the equally spaced grid
 
     specs.n_perm_shocks = 24;
-    specs.n_trans_shocks = 15;
+    specs.n_trans_shocks = 5;
 
     specs.time_series = 100000; % length of the time series for each perm type
     specs.N_obs = 25000; % grab last number of observations
